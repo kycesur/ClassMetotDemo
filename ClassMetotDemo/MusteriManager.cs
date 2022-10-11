bd0448d32c88649cc841da0a926e1f08045a5381
Kadir <Kadir@DESKTOP-98TS8Q8>
2022-10-11 21:54:03 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    { public void MusteriEkle (Musteri musteri)
+            {
+
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Eklendi.");
+
+            }
+        public void MusteriSil (Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " silindi.");
+
+        }
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri  in musteriler)
+            {
+                Console.WriteLine(musteri.Ad +" "+ musteri.Soyad );
+            }
+            Console.WriteLine(musteriler.Count()+" adet kişi listelendi."); 
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+{
+    musteri1.Ad = "Kadir";
+    musteri1.Soyad = "Cesur";
+    musteri1.Yas = 24;
+
+}
+Musteri musteri2 = new Musteri();
+{
+    musteri2.Ad = "Ferhat";
+    musteri2.Soyad = "Efe";
+    musteri2.Yas = 23;
+}
+Musteri musteri3 = new Musteri();
+{
+    musteri3.Ad = "Mustafa";
+    musteri3.Soyad = "Soykal";
+    musteri3.Yas = 22;
+}
+MusteriManager Musteri= new MusteriManager();
+
+Musteri.MusteriEkle(musteri1);
+Musteri.MusteriEkle(musteri2);
+Musteri.MusteriEkle(musteri3);
+Musteri.MusteriSil(musteri3);
+Musteri[] musteris = new Musteri[]
+{
+    musteri1,musteri2
+};
+Musteri.MusteriListele(musteris);
+
+
+
